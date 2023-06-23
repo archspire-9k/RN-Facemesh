b697cfc47a38167a82709155104b0d0087897f45
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-06-23 04:03:20 -0400

    Fabric module migration

--- App.tsx
+++ App.tsx
@@ -8,24 +8,17 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import NewModuleButton from './src/components/NewModuleButton';
+import RTNFaceLandmarker from 'rtn-face-landmarker/js/RTNFaceLandmarkerNativeComponent';
 
 function App(): JSX.Element {
   const [cameraOn, setCameraOn] = useState(false)
 
   if (!cameraOn) {
     return (
-      <View style = {{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}>
-      <View>
-          <TouchableOpacity onPress={() => setCameraOn(true)} style={{ marginVertical: 30, backgroundColor: '#28a745' }}>
-            <Text style={{ marginHorizontal: 20, marginVertical: 10, color: 'white', textAlign: 'center' }}>ON</Text>
-          </TouchableOpacity>
-          <NewModuleButton/>
-        </View>
-        </View>
+      <RTNFaceLandmarker
+            text='History'
+            style={{ width: '100%', height: 30 }}
+          />
     );
   }
 
